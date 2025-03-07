a65f53c5711050a7bbfc0d994a1f0e6979309db0
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2025-03-07 12:50:59 +0100

    backend docker ready

--- Backend/FILEMANAGER/file_manager/filemanager/file_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from filemanager.models import StoredFile
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StoredFile
-        fields = ["id", "file", "uploaded_at"]
-   
