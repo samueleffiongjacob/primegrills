d3b3a673d9e0721ff4cd004d2cf2dd7763e2c94e
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2025-03-17 02:18:01 +0100

    mailtrap

--- Backend/AUTH/auth/auth/settings.py
+++ Backend/AUTH/auth/auth/settings.py
@@ -201,9 +201,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'live.smtp.mailtrap.io'  # Change to the correct SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'api'  # Change to correct email
-EMAIL_HOST_PASSWORD = '08876421370cc7b9f2f5b5782eb9cddb'  # Change to correct email password or app password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  #'Prime Grills <no-reply@primegrills.com>'
+EMAIL_HOST_USER = 'api, smtp@mailtrap.io'  # Change to correct email
+DOMAIN ='effiongsamuel.tech'
+EMAIL_HOST_PASSWORD = 'e4b751d9f0c3495f4396913a5e813054'  # Change to correct email password or app password
+DEFAULT_FROM_EMAIL = 'Prime Grills <no-reply@effiongsamuel.tech>'#EMAIL_HOST_USER  #'Prime Grills <no-reply@primegrills.com>'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
 
 
 # JWT settings
